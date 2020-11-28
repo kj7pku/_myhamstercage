e668fb5044816f858e2b69476a3119cae8eb2474
steven <spokra@gmail.com>
2020-11-28 13:08:24 -0800

    working submition

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://root:QbpbVYoD6g@mongodb-1604800654.default.svc.cluster.local/";
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MyHamsterCage.com' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'MyHamsterCage.com'});
+});
+router.post('/', function (req, res) {
+    if (!req.body || Object.keys(req.body).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let licenseFile = req.files.fcc.data;
+    let email = req.body.email;
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("Licenses");
+        var myobj = {email: email, LicensePDF: licenseFile, State: 0, LicenseTXT: ''};
+        dbo.collection("queue").insertOne(myobj, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+
+    res.send(res.render('complete', {title: 'MyHamsterCage.com', email: email}));
 });
 
 module.exports = router;
